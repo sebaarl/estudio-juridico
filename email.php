156e0e1dde4b8a97efdd3792e8f749665de17fbf
Sebastian Rivera <se.riveral@duocuc.cl>
2021-08-25 23:23:36 -0400

    Cambios en email.php

--- email.php
+++ email.php
@@ -6,13 +6,13 @@ $message = $_POST['message'];
 
 $to = "rsebadb@gmail.com";
 
-$subject = "De: ".$emailFrom;
-$txt = "Nombre: ". $name . "\r\n Email: ". $emailFrom . "Asunto: " . $subject .  "\r\n Mensaje: ". $message . "\r\n";
+$subjectEmail = "De: ".$emailFrom;
+$txt = "Nombre: ". $name . "\r\n Email: ". $emailFrom . "\r\n Asunto: " . $subject .  "\r\n Mensaje: ". $message . "\r\n";
 
 $headers = "From: noreply@zunigahurtadoasoc.com" . "\r\n" . "CC: ". $emailFrom . "\r\n"; 
 
 if ($emailFrom!=NULL) {
-    mail($to, $subject, $txt, $headers);
+    mail($to, $subjectEmail, $txt, $headers);
 }
 
 header("Location:mensaje-enviado.html");
